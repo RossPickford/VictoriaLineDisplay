093b556593bea45a12d4889da02a2e068fc1f13d
VND Pickford <Pickford.Ross@bcg.com>
2026-06-05 14:43:39 +0100

    whyy it be like dis. Libcurl not working :(

--- libcurlTest.c
+++ libcurlTest.c
@@ -1,58 +1,40 @@
-/***************************************************************************
- *                                  _   _ ____  _
- *  Project                     ___| | | |  _ \| |
- *                             / __| | | | |_) | |
- *                            | (__| |_| |  _ <| |___
- *                             \___|\___/|_| \_\_____|
- *
- * Copyright (C) Daniel Stenberg, <daniel@haxx.se>, et al.
- *
- * This software is licensed as described in the file COPYING, which
- * you should have received as part of this distribution. The terms
- * are also available at https://curl.se/docs/copyright.html.
- *
- * You may opt to use, copy, modify, merge, publish, distribute and/or sell
- * copies of the Software, and permit persons to whom the Software is
- * furnished to do so, under the terms of the COPYING file.
- *
- * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
- * KIND, either express or implied.
- *
- * SPDX-License-Identifier: curl
- *
- ***************************************************************************/
-/* <DESC>
- * Simple HTTP GET
- * </DESC>
- */
 #include <stdio.h>
- 
-#include <curl/curl.h>
- 
+#include "curl/curl.h"
+
 int main(void)
 {
+  printf("hello!\n");
   CURL *curl;
- 
-  CURLcode result = curl_global_init(CURL_GLOBAL_WIN32);
-  if(result != CURLE_OK)
-    return (int)result;
- 
-  curl = curl_easy_init();
-  if(curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "https://api.tfl.gov.uk/Line/victoria/Arrivals");
-    /* example.com is redirected, so we tell libcurl to follow redirection */
-    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
- 
-    /* Perform the request, result gets the return code */
-    result = curl_easy_perform(curl);
-    /* Check for errors */
-    if(result != CURLE_OK)
-      fprintf(stderr, "curl_easy_perform() failed: %s\n",
-              curl_easy_strerror(result));
- 
-    /* always cleanup */
-    curl_easy_cleanup(curl);
+
+  // CURLcode result = curl_global_init(CURL_GLOBAL_ALL);
+  CURLcode result;
+  if (result != CURLE_OK)
+  {
+    printf("welp");
+    // return (int)result;
   }
-  curl_global_cleanup();
+
+  curl = curl_easy_init();
+  if (!curl)
+  {
+    fprintf(stderr, "failed to easy init");
+    return -1;
+  }
+
+  /* curl_easy_setopt(curl, CURLOPT_URL, "https://api.tfl.gov.uk/Line/victoria/Arrivals");
+  // example.com is redirected, so we tell libcurl to follow redirection
+  // curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+
+  // Perform the request, result gets the return code
+  result = curl_easy_perform(curl);
+  // Check for errors
+  if (result != CURLE_OK)
+    fprintf(stderr, "curl_easy_perform() failed: %s\n",
+            curl_easy_strerror(result));
+
+  // always cleanup
+  curl_easy_cleanup(curl);
+ */
+  // curl_global_cleanup();
   return 0;
 }

47aa6bb1ab5dbc939a0b2cd9dd2e05f1e7ab3adc
VND Pickford <Pickford.Ross@bcg.com>
2026-07-03 16:38:13 +0100

    trying a different approach to gathering data - less requests, but will need to time it first

--- request.py
+++ request.py
@@ -1,92 +1,43 @@
 import time
 start = time.time()
 
-station_id_lookup = { "Stockwell" : "940GZZLUSKW",
-                     "Vauxhall" : "940GZZLUVXL",
-                     "Pimlico" : "940GZZLUPCO",
-                     "Victoria" : "940GZZLUVIC",
-                     "Green Park" : "940GZZLUGPK",
-                     "Oxford Circus" : "940GZZLUOXC",
-                     "Warren Street" : "940GZZLUWRR",
-                     "Euston" : "940GZZLUEUS",
-                     "Kings Cross St. Pancras" : "940GZZLUKSX",
-                     "Kings Cross St. P" : "940GZZLUKSX",
-                     "Highbury & Islington" : "940GZZLUHAI",
-                     "Highbury & Isl" : "940GZZLUHAI",
-                     "Finsbury Park" : "940GZZLUFPK",
-                     "Seven Sisters" : "940GZZLUSVS",
-                     "Tottenham Hale" : "940GZZLUTMH",
-                     "Blackhorse Road" : "940GZZLUBLR"}
+station_ids = { "Brixton" : "940GZZLUBXN",
+                "Stockwell" : "940GZZLUSKW",
+                "Vauxhall" : "940GZZLUVXL",
+                "Pimlico" : "940GZZLUPCO",
+                "Victoria" : "940GZZLUVIC",
+                "Green Park" : "940GZZLUGPK",
+                "Oxford Circus" : "940GZZLUOXC",
+                "Warren Street" : "940GZZLUWRR",
+                "Euston" : "940GZZLUEUS",
+                "Kings Cross St. Pancras" : "940GZZLUKSX",
+                "Highbury & Islington" : "940GZZLUHAI",
+                "Finsbury Park" : "940GZZLUFPK",
+                "Seven Sisters" : "940GZZLUSVS",
+                "Tottenham Hale" : "940GZZLUTMH",
+                "Blackhorse Road" : "940GZZLUBLR",
+                "Walthamstow Central" : "940GZZLUWWL"}
+
+
 
 class TrainData:
-    def __init__(self, nextStation, timeToStation, state):
+    def __init__(self, direction, nextStation, timeToStation, state):
+        self.direction = direction
         self.nextStation = nextStation
         self.timeToStation = timeToStation
         self.state = state
 
 import subprocess
-# subprocess.run(["curl", "https://api.tfl.gov.uk/Line/victoria/Arrivals/", "-o", "trains.json"])
-subprocess.run(["curl", "https://api.tfl.gov.uk/Line/victoria/Arrivals/940GZZLUBXN", "-o", "southbound.json"])
-subprocess.run(["curl", "https://api.tfl.gov.uk/Line/victoria/Arrivals/940GZZLUWWL", "-o", "northbound.json"])
+subprocess.run(["curl", "https://api.tfl.gov.uk/Line/victoria/Arrivals/", "-o", "trains.json"])
+# subprocess.run(["curl", "https://api.tfl.gov.uk/Line/victoria/Arrivals/940GZZLUBXN", "-o", "southbound.json"])
+# subprocess.run(["curl", "https://api.tfl.gov.uk/Line/victoria/Arrivals/940GZZLUWWL", "-o", "northbound.json"])
 
 from sys import path
 import link
 path.append(link.getRequestsAddress())
 
-train_data = {}
-train_list = []
-
 from requests import get
 import nextStation
-
-"""API_KEY = "580efc09100e43b6976feaefb4e33f12"
-url = "https://api.tfl.gov.uk/Line/victoria/Arrivals"
-params = {"app_key": API_KEY}
-
-response = get(url, params=params)
-data = response.json()
-
-print("Trains: ")
-for train in data:
-    id = train.get("vehicleId", "Unknown")
-    if id not in trainList:
-        trainList.append(id)
-    location = train.get("currentLocation", "No location available")
-    stationName = train.get("stationName", "No Station Available")
-    stationName = stationName.replace(" Underground Station", "")
-    direction = train.get("towards", "No Direction Available")
-    if direction == "Brixton":
-        direction = "southbound"
-    else:
-        direction = "northbound"
-    if "Leaving" in location or "Left" in location:
-        print("train Leaving")
-    elif "Between" in location or "Approaching" in location:
-
-        time = train.get("timeToStation", "Unknown")
-        if stationName in location:
-            if id in trainData:
-                if trainData[id][3] > time:
-                     trainData[id][3] = time
-            else:
-                trainData.update({id : [stationName, direction, location, time]})
-    elif "At" in location or "At Platform" == location:
-        if stationName in location:
-            if id in trainData:
-                if direction == "northbound":
-                    _nextStation = nextStation.northbound[stationName]
-                else:
-                    _nextStation = nextStation.southbound[stationName]
-
-                if trainData[id][0] != _nextStation:
-                    trainData[id] = [stationName, direction, location, 0]
-            else:
-                trainData[id] = [stationName, direction, location, 0]
-
-import trainTimes
-
-for train_id, data in trainData.items():
-    print(f"Train {train_id} → {data[0]} | direction: {data[1]} | location: {data[2]} | time to station: {data[3]} / {trainTimes.getStationTime(data[1], data[0])}") """
 
 def getStationFromLocation(location):
     buf = []
@@ -107,25 +58,21 @@ def getStationFromLocation(location):
 
 API_KEY = "580efc09100e43b6976feaefb4e33f12"
 base_url = "https://api.tfl.gov.uk/Line/victoria/Arrivals/"
-northbound_url = "https://api.tfl.gov.uk/Line/victoria/Arrivals/940GZZLUWWL"
-southbound_url = "https://api.tfl.gov.uk/Line/victoria/Arrivals/940GZZLUBXN"
 params = {"app_key": API_KEY}
 
-northbound_response = get(northbound_url, params=params)
-northbound_data = northbound_response.json()
+train_data = {}
 
-southbound_response = get(southbound_url, params=params)
-southbound_data = southbound_response.json()
+count = 0
+for station_name, stop_id in station_ids.items():
+    url = base_url + stop_id
+    request = get(url, params=params)
+    data = request.json()
 
-north_trains = {}
-south_trains = {}
-all_data = [[northbound_data, north_trains], [southbound_data, south_trains]]
+    temp_trainData = {}
 
-
-for data in all_data:
-    for train in data[0]:
+    for train in data:
         id = train.get("vehicleId", "No vehicle Id")
-        if not id.isdigit():
+        if not id.isdigit() or id in train_data.keys():
             continue
 
         location = train.get("currentLocation", "Location unknown")
@@ -135,49 +82,55 @@ for data in all_data:
         if "Between" in location:
             station = getStationFromLocation(location)
             state = "moving"
+            timeToStation = train.get("timeToStation", "No time given")
         elif "Approaching" in location:
             station = location.replace("Approaching ", "")
             state = "moving"
+            timeToStation = train.get("timeToStation", "No time given")
         elif "At Platform" in location:
-            station = (train.get("stationName", "no station available")).replace(" Underground Station", "")
+            station = station_name
             state = "idle"
         elif "At" in location:
             station = location.replace("At ", "")
             state = "idle"
+        elif "Departing" in location or "Departed" in location or "Left" in location:    
+            station = list(station_ids.keys())[count - 1] if station_name == "Walthamstow Central" else list(station_ids.keys())[count + 1]
+            state = "moving"
+            print(f"{id} : {location} : {station}")
+        elif "Area" in location and "Brixton" in location:
+            station = "Brixton"
+            state = "moving"
+        else:
+            print(f"Unknown Location Value: {location}")
 
-        station_url = base_url
-        if state == "moving" and station in station_id_lookup.keys():
-            timeToStation = 100000
-            station_url += station_id_lookup[station]
-            station_url += "?direction="
-            station_url += "outbound" if data[0] == northbound_data else "inbound"
-            station_request = get(station_url, params=params)
-            station_data = station_request.json()
-            for s in station_data:
-                if id == s.get("vehicleId", "no id"):
-                    t = s.get("timeToStation", "No Time Given")
-                    if t < timeToStation:
-                        timeToStation = t
-        elif state == "moving" and (station == "Walthamstow Central" or station == "Brixton"):
-            timeToStation = train.get("timeToStation", "No Time Given")
-            
-        if id not in data[1]:
-            data[1].update({id : TrainData(station, timeToStation, state)})
-        elif data[1][id].nextStation != station:
+        if station == "Highbury & Isl":
+            station = "Highbury & Islington"
+        elif station == "Kings Cross St. P":
+            station = "Kings Cross St. Pancras"
+
+        if station != station_name:
+            continue
+
+        direction = train.get("towards","No direction")
+
+        direction = "northbound" if direction == "Walthamstow Central" else "southbound"
+
+        if id not in temp_trainData.keys():
+            temp_trainData.update({id : TrainData(direction, station, timeToStation, state)})
+        elif temp_trainData[id].nextStation != station:
             print(f"Error 01: Different Location for train {id}")
-        elif timeToStation < data[1][id].timeToStation: 
-                data[1][id].timeToStation = timeToStation
+        elif timeToStation < temp_trainData[id].timeToStation: 
+                temp_trainData[id].timeToStation = timeToStation
+    
+    train_data.update(temp_trainData)
+    count += 1
 
 
 import trainTimes
 
-print("Northbound Trains")
-for id, loc in north_trains.items():
-        print(f"ID: {id} | {"next" if loc.state == "moving" else "current"} Stop: {loc.nextStation} | time to: {loc.timeToStation} / {trainTimes.getStationTime("northbound", loc.nextStation)}")
-
-print("Southbound Trains")
-for id, loc in south_trains.items():
-     print(f"ID: {id} | {"next" if loc.state == "moving" else "current"} Stop: {loc.nextStation} | time to: {loc.timeToStation} / {trainTimes.getStationTime("southbound", loc.nextStation)}")
+print("Trains")
+for id, loc in train_data.items():
+        print(f"ID: {id} | {loc.direction} | {"next" if loc.state == "moving" else "current"} Stop: {loc.nextStation} | time to: {loc.timeToStation} / {trainTimes.getStationTime(loc.direction, loc.nextStation)}")
 
 end = time.time()
 

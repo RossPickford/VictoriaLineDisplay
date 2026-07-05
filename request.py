f0ecc97bf231e4fb505b9c657504930255cb5761
Ross Pickford <ross.pickford34@gmail.com>
2026-07-05 17:41:06 +0100

    removed commented out code

--- request.py
+++ request.py
@@ -26,8 +26,8 @@ class TrainData:
 
 import subprocess
 # subprocess.run(["curl", "https://api.tfl.gov.uk/Line/victoria/Arrivals/", "-o", "trains.json"])
-subprocess.run(["curl", "https://api.tfl.gov.uk/Line/victoria/Arrivals/940GZZLUBXN", "-o", "southbound.json"])
-subprocess.run(["curl", "https://api.tfl.gov.uk/Line/victoria/Arrivals/940GZZLUWWL", "-o", "northbound.json"])
+# subprocess.run(["curl", "https://api.tfl.gov.uk/Line/victoria/Arrivals/940GZZLUBXN", "-o", "southbound.json"])
+# subprocess.run(["curl", "https://api.tfl.gov.uk/Line/victoria/Arrivals/940GZZLUWWL", "-o", "northbound.json"])
 
 from sys import path
 import link
@@ -38,55 +38,6 @@ train_list = []
 
 from requests import get
 import nextStation
-
-"""API_KEY = "580efc09100e43b6976feaefb4e33f12"
-url = "https://api.tfl.gov.uk/Line/victoria/Arrivals"
-params = {"app_key": API_KEY}
-
-response = get(url, params=params)
-data = response.json()
-
-print("Trains: ")
-for train in data:
-    id = train.get("vehicleId", "Unknown")
-    if id not in trainList:
-        trainList.append(id)
-    location = train.get("currentLocation", "No location available")
-    stationName = train.get("stationName", "No Station Available")
-    stationName = stationName.replace(" Underground Station", "")
-    direction = train.get("towards", "No Direction Available")
-    if direction == "Brixton":
-        direction = "southbound"
-    else:
-        direction = "northbound"
-    if "Leaving" in location or "Left" in location:
-        print("train Leaving")
-    elif "Between" in location or "Approaching" in location:
-
-        time = train.get("timeToStation", "Unknown")
-        if stationName in location:
-            if id in trainData:
-                if trainData[id][3] > time:
-                     trainData[id][3] = time
-            else:
-                trainData.update({id : [stationName, direction, location, time]})
-    elif "At" in location or "At Platform" == location:
-        if stationName in location:
-            if id in trainData:
-                if direction == "northbound":
-                    _nextStation = nextStation.northbound[stationName]
-                else:
-                    _nextStation = nextStation.southbound[stationName]
-
-                if trainData[id][0] != _nextStation:
-                    trainData[id] = [stationName, direction, location, 0]
-            else:
-                trainData[id] = [stationName, direction, location, 0]
-
-import trainTimes
-
-for train_id, data in trainData.items():
-    print(f"Train {train_id} → {data[0]} | direction: {data[1]} | location: {data[2]} | time to station: {data[3]} / {trainTimes.getStationTime(data[1], data[0])}") """
 
 def getStationFromLocation(location):
     buf = []

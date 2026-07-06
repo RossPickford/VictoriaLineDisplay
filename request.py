16c0d774ef0c2f0aa668ace00f0d1565a368935c
VND Pickford <Pickford.Ross@bcg.com>
2026-07-06 16:20:44 +0100

    It seems to be working now :)

--- request.py
+++ request.py
@@ -1,5 +1,5 @@
 import time
-start = time.time()
+totalTime_init = time.time()
 
 station_ids = { "Brixton" : "940GZZLUBXN",
                 "Stockwell" : "940GZZLUSKW",
@@ -17,8 +17,6 @@ station_ids = { "Brixton" : "940GZZLUBXN",
                 "Tottenham Hale" : "940GZZLUTMH",
                 "Blackhorse Road" : "940GZZLUBLR",
                 "Walthamstow Central" : "940GZZLUWWL"}
-
-
 
 class TrainData:
     def __init__(self, direction, nextStation, timeToStation, state):
@@ -62,25 +60,34 @@ params = {"app_key": API_KEY}
 
 train_data = {}
 
+
+totalTime_request = 0
+totalTime_trainDataCollection = 0
+totalTime_all = time.time()
+totalTime_init = time.time() - totalTime_init
+
 count = 0
 for station_name, stop_id in station_ids.items():
-    # request_start = time.time()
+    request_start = time.time()
+    
     url = base_url + stop_id
     request = get(url, params=params)
     data = request.json()
-    # request_end = time.time()
 
-    # print(f"Request took: {request_end - request_start}")
+    totalTime_request += time.time() - request_start
 
     temp_trainData = {}
-
     for train in data:
+        dataCollection_start = time.time()
+
         id = train.get("vehicleId", "No vehicle Id")
-        if not id.isdigit() or id in train_data.keys():
+        if not id.isdigit():
+            print("Invalid train id")
+            continue
+        if id in train_data.keys():
             continue
 
         location = train.get("currentLocation", "Location unknown")
-        # print(location)
         station = location            
         timeToStation = 0
         state = "None"
@@ -102,10 +109,11 @@ for station_name, stop_id in station_ids.items():
             destination = train.get("towards", "no destination")
             a = 1 if destination == "Brixton" else -1
             previousStation = list(station_ids.keys())[count + a]
-            print(f"{id} | {destination} : {location} : {previousStation} : {station_name}")
+            # print(f"{id} | {destination} : {location} : {previousStation} : {station_name}")
             if previousStation in location:
                 station = station_name
-                state = "moving"
+                state = 1
+                timeToStation = train.get("timeToStation", "No time given")
             else:
                 continue
         elif "Area" in location and "Brixton" in location:
@@ -132,20 +140,26 @@ for station_name, stop_id in station_ids.items():
             print(f"Error 01: Different Location for train {id}")
         elif timeToStation < temp_trainData[id].timeToStation: 
                 temp_trainData[id].timeToStation = timeToStation
+
+        totalTime_trainDataCollection += time.time() - dataCollection_start
     
     train_data.update(temp_trainData)
     count += 1
 
+totalTime_all = time.time() - totalTime_all
 
 import trainTimes
 
-print("Trains")
+print("\n==============Trains==============\n")
 for id, loc in train_data.items():
         print(f"ID: {id} | {loc.direction} | {"next" if loc.state == "moving" else "current"} Stop: {loc.nextStation} | time to: {loc.timeToStation} / {trainTimes.getStationTime(loc.direction, loc.nextStation)}")
 
 end = time.time()
 
-print(f"Program took {end - start} seconds to run")
+print(f"\nPre-initialisation took {totalTime_init} seconds")
+print(f"Main program took {totalTime_all} seconds to run")
+print(f"Requests took {totalTime_request} seconds")
+print(f"Data collection took {totalTime_trainDataCollection} seconds\n")
 
 
 
